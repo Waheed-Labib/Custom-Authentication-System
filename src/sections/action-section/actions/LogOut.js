ec8415c0fede07dcb0ddd2c45d1f00aebb7cfb71
Waheed-Labib <labibwahid28@gmail.com>
2024-02-03 15:41:17 +0600

    added log out button styles

--- src/sections/action-section/actions/LogOut.js
+++ src/sections/action-section/actions/LogOut.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const LogOut = () => {
     return (
-        <div>
-            Log Out
+        <div className='min-h-[50vh] md:min-h-screen bg-base-200 flex items-center justify-center'>
+            <button className='bg-red-500 hover:bg-red-600 text-white font-semibold py-4 px-5 rounded'>Log Out</button>
         </div>
     );
 };
--- src/sections/welcome-section/WelcomeSection.js
+++ src/sections/welcome-section/WelcomeSection.js
@@ -16,17 +16,17 @@ const WelcomeSection = () => {
     }
 
     return (
-        <div className='w-full md:min-h-screen flex items-center justify-center' >
+        <div className='w-full mt-[30%] md:mt-0 md:min-h-screen flex items-center justify-center' >
             {
                 user ?
                     <div className='w-2/3'>
                         <h2 className='text-4xl font-bold text-white'>{welcomeUser.header}</h2>
-                        <p className='mt-3 text-justify'>{welcomeUser.paragraph}</p>
+                        <p className='mt-3'>{welcomeUser.paragraph}</p>
                     </div>
                     :
                     <div className='w-2/3'>
                         <h2 className='text-4xl font-bold text-white'>{welcomeGuest.header}</h2>
-                        <p className='mt-3 text-justify'>{welcomeGuest.paragraph}</p>
+                        <p className='mt-3'>{welcomeGuest.paragraph}</p>
                     </div>
 
             }
